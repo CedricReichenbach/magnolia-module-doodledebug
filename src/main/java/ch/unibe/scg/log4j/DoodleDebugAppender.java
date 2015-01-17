c5f6451503b6db978d7807fce4af1ee6c6d91cf2
Cedric Reichenbach <cedric.reichenbach@students.unibe.ch>
2015-01-17 18:25:35 +0100

    Workaround for circular dependency and curcurrency problems

--- src/main/java/ch/unibe/scg/log4j/DoodleDebugAppender.java
+++ src/main/java/ch/unibe/scg/log4j/DoodleDebugAppender.java
@@ -1,11 +1,16 @@
 package ch.unibe.scg.log4j;
 
 import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.MDC;
 import org.apache.log4j.spi.LoggingEvent;
 
 import ch.unibe.scg.doodle.Doo;
 
 public class DoodleDebugAppender extends AppenderSkeleton {
+
+	private static final String IN_APPEND_KEY = DoodleDebugAppender.class
+			.getName() + ".inAppend";
+	private static boolean ddLock = false;
 
 	public DoodleDebugAppender() {
 		super();
@@ -26,18 +31,34 @@ public class DoodleDebugAppender extends AppenderSkeleton {
 	}
 
 	@Override
-	protected void append(LoggingEvent event) {
-		// Doo.dle(event);
-		Doo.dle(event.getMessage());
+	protected void append(final LoggingEvent event) {
+		// prevent cycle (because DD indirectly uses log4j, too)
+		if (event.getMDC(IN_APPEND_KEY) != null)
+			return;
+		MDC.put(IN_APPEND_KEY, this);
 
-		// workaround to avoid circular dependency
-		// try {
-		// Method dle = Class.forName("ch.unibe.scg.doodle.Doo").getMethod(
-		// "dle", Object.class);
-		// dle.invoke(null, event.getMessage());
-		// } catch (Exception e) {
-		// e.printStackTrace();
-		// }
+		try {
+			// XXX: DD is not thread safe
+			 Runnable doodlingRunnable = new Runnable() {
+				public void run() {
+					while (ddLock)
+						try {
+							Thread.sleep(10);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+							return;
+						}
+					
+					ddLock = true;
+					// Doo.dle(event);
+					Doo.dle(event.getMessage());
+					ddLock = false;
+				}
+			};
+			new Thread(doodlingRunnable).start();
+		} finally {
+			MDC.remove(IN_APPEND_KEY);
+		}
 	}
 
 }
