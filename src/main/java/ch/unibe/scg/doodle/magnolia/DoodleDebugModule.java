57332dfb6258573cffc56d479ed861ce7c66a6f4
Cedric Reichenbach <cedric.reichenbach@students.unibe.ch>
2015-06-06 21:01:41 +0200

    Start webapp on module init and use JCR databse

--- src/main/java/ch/unibe/scg/doodle/magnolia/DoodleDebugModule.java
+++ src/main/java/ch/unibe/scg/doodle/magnolia/DoodleDebugModule.java
@@ -1,12 +1,43 @@
 package ch.unibe.scg.doodle.magnolia;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ch.unibe.scg.doodle.DoodleDebug;
+import ch.unibe.scg.doodle.DoodleDebugWebapp;
+import ch.unibe.scg.doodle.magnolia.jcr.JcrDatabase;
+import info.magnolia.module.ModuleLifecycle;
+import info.magnolia.module.ModuleLifecycleContext;
+
 /**
- * This class is optional and represents the configuration for the doodledebug module.
- * By exposing simple getter/setter/adder methods, this bean can be configured via content2bean
- * using the properties and node from <tt>config:/modules/doodledebug</tt>.
- * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ * This class is optional and represents the configuration for the doodledebug
+ * module. By exposing simple getter/setter/adder methods, this bean can be
+ * configured via content2bean using the properties and node from
+ * <tt>config:/modules/doodledebug</tt>. If you don't need this, simply remove
+ * the reference to this class in the module descriptor xml.
  */
-public class DoodleDebugModule {
-    /* you can optionally implement info.magnolia.module.ModuleLifecycle */
+public class DoodleDebugModule implements ModuleLifecycle {
+
+	private static final int PORT = 3180;
+
+	private static final Logger log = LoggerFactory
+			.getLogger(DoodleDebugModule.class);
+
+	@Override
+	public void start(ModuleLifecycleContext ctx) {
+		DoodleDebug.setDatabaseMap(JcrDatabase.class);
+		
+		log.info("Starting DoodleDebugWebapp server...");
+		try {
+			DoodleDebugWebapp.startServer(PORT);
+		} catch (Exception e) {
+			log.error("Failed to start DoodleDebugWebapp server", e);
+		}
+	}
+
+	@Override
+	public void stop(ModuleLifecycleContext ctx) {
+		// XXX: Stop server?
+	}
 
 }
